74f4787e64fccc10a5c935428fcb2ef0038fbe31
Trudy <trudy@vpnhood.com>
2024-07-28 00:34:02 -0700

    publish nuget

--- DotNet/SampleShowAd/MainActivity.cs
+++ DotNet/SampleShowAd/MainActivity.cs
@@ -1,34 +1,34 @@
+using _Microsoft.Android.Resource.Designer;
 using VpnHood.Client.App.Droid.Ads.VhInMobi;
 using VpnHood.Client.Device.Droid;
 using VpnHood.Client.Device.Droid.ActivityEvents;
 
-namespace SampleShowAd
+namespace SampleShowAd;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : ActivityEvent
 {
-    [Activity(Label = "@string/app_name", MainLauncher = true)]
-    public class MainActivity : ActivityEvent
+    protected override void OnCreate(Bundle? savedInstanceState)
     {
-        protected override void OnCreate(Bundle? savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
+        base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
-            _ = ShowAd();
+        // Set our view from the "main" layout resource
+        SetContentView(ResourceConstant.Layout.activity_main);
+        _ = ShowAd();
+    }
+
+    private async Task ShowAd()
+    {
+        try
+        {
+            var adService = InMobiService.Create(InMobiCredential.AccountId, InMobiCredential.PlacementId, true);
+            await adService.LoadAd(new AndroidUiContext(this), new CancellationToken());
+            await adService.ShowAd(new AndroidUiContext(this), new CancellationToken());
         }
-
-        private async Task ShowAd()
+        catch (Exception e)
         {
-            try
-            {
-                var adService = InMobiService.Create(InMobiCredential.AccountId, InMobiCredential.PlacementId, true);
-                await adService.LoadAd(new AndroidUiContext(this), new CancellationToken());
-                await adService.ShowAd(new AndroidUiContext(this), new CancellationToken());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            Console.WriteLine(e);
+            throw;
         }
     }
 }
--- DotNet/VhInMobi/InMobiService.cs
+++ DotNet/VhInMobi/InMobiService.cs
@@ -5,83 +5,82 @@ using VpnHood.Client.Device;
 using VpnHood.Client.Device.Droid;
 using VpnHood.Common.Exceptions;
 
-namespace VpnHood.Client.App.Droid.Ads.VhInMobi
+namespace VpnHood.Client.App.Droid.Ads.VhInMobi;
+
+public class InMobiService(string accountId, long placementId, bool isDebugMode)
 {
-    public class InMobiService(string accountId, long placementId, bool isDebugMode)
+    private Com.Vpnhood.Inmobi.Ads.IAppAdService? _vhInMobiAdService;
+
+    public string NetworkName => "InMobi";
+    public AppAdType AdType => AppAdType.InterstitialAd;
+    public DateTime? AdLoadedTime { get; private set; }
+    public TimeSpan AdLifeSpan { get; } = TimeSpan.FromMinutes(45);
+
+    public static InMobiService Create(string accountId, long placementId, bool isDebugMode)
     {
-        private Com.Vpnhood.Inmobi.Ads.IAppAdService? _vhInMobiAdService;
+        var ret = new InMobiService(accountId, placementId, isDebugMode);
+        return ret;
+    }
 
-        public string NetworkName => "InMobi";
-        public AppAdType AdType => AppAdType.InterstitialAd;
-        public DateTime? AdLoadedTime { get; private set; }
-        public TimeSpan AdLifeSpan { get; } = TimeSpan.FromMinutes(45);
+    public bool IsCountrySupported(string countryCode)
+    {
+        // Make sure it is upper case
+        countryCode = countryCode.Trim().ToUpper();
 
-        public static InMobiService Create(string accountId, long placementId, bool isDebugMode)
+        // these countries are not supported at all
+        return true;
+    }
+
+    public async Task LoadAd(IUiContext uiContext, CancellationToken cancellationToken)
+    {
+        var appUiContext = (AndroidUiContext)uiContext;
+        var activity = appUiContext.Activity;
+        if (activity.IsDestroyed)
+            throw new AdException("MainActivity has been destroyed before loading the ad.");
+
+        // reset the last loaded ad
+        AdLoadedTime = null;
+
+        // initialize
+        await InMobiUtil.Initialize(activity, accountId, isDebugMode, cancellationToken);
+        _vhInMobiAdService = InMobiAdServiceFactory.Create(Java.Lang.Long.ValueOf(placementId)) 
+                             ?? throw new AdException($"The {AdType} ad is not initialized");
+
+        await _vhInMobiAdService.LoadAd(activity)!.AsTask();
+        AdLoadedTime = DateTime.Now;
+    }
+
+    public async Task ShowAd(IUiContext uiContext, CancellationToken cancellationToken)
+    {
+        var appUiContext = (AndroidUiContext)uiContext;
+        var activity = appUiContext.Activity;
+        if (activity.IsDestroyed)
+            throw new AdException("MainActivity has been destroyed before showing the ad.");
+
+        try
         {
-            var ret = new InMobiService(accountId, placementId, isDebugMode);
-            return ret;
+            if (AdLoadedTime == null || _vhInMobiAdService == null)
+                throw new AdException($"The {AdType} has not been loaded.");
+
+            Task? task = null;
+            // wait for show or dismiss
+            activity.RunOnUiThread(() =>
+            {
+                task = _vhInMobiAdService.ShowAd(activity)!.AsTask();
+            });
+
+            if (task != null)
+                await task.ConfigureAwait(false);
         }
-
-        public bool IsCountrySupported(string countryCode)
+        finally
         {
-            // Make sure it is upper case
-            countryCode = countryCode.Trim().ToUpper();
-
-            // these countries are not supported at all
-            return true;
-        }
-
-        public async Task LoadAd(IUiContext uiContext, CancellationToken cancellationToken)
-        {
-            var appUiContext = (AndroidUiContext)uiContext;
-            var activity = appUiContext.Activity;
-            if (activity.IsDestroyed)
-                throw new AdException("MainActivity has been destroyed before loading the ad.");
-
-            // reset the last loaded ad
+            _vhInMobiAdService = null;
             AdLoadedTime = null;
-
-            // initialize
-            await InMobiUtil.Initialize(activity, accountId, isDebugMode, cancellationToken);
-            _vhInMobiAdService = InMobiAdServiceFactory.Create(Java.Lang.Long.ValueOf(placementId)) 
-                ?? throw new AdException($"The {AdType} ad is not initialized");
-
-            await _vhInMobiAdService.LoadAd(activity)!.AsTask();
-            AdLoadedTime = DateTime.Now;
         }
+    }
 
-        public async Task ShowAd(IUiContext uiContext, CancellationToken cancellationToken)
-        {
-            var appUiContext = (AndroidUiContext)uiContext;
-            var activity = appUiContext.Activity;
-            if (activity.IsDestroyed)
-                throw new AdException("MainActivity has been destroyed before showing the ad.");
-
-            try
-            {
-                if (AdLoadedTime == null || _vhInMobiAdService == null)
-                    throw new AdException($"The {AdType} has not been loaded.");
-
-                Task? task = null;
-                // wait for show or dismiss
-                activity.RunOnUiThread(() =>
-                {
-                    task = _vhInMobiAdService.ShowAd(activity)!.AsTask();
-                });
-
-                if (task != null)
-                    await task.ConfigureAwait(false);
-            }
-            finally
-            {
-                _vhInMobiAdService = null;
-                AdLoadedTime = null;
-            }
-        }
-
-        public void Dispose()
-        {
-            GC.SuppressFinalize(this);
-        }
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
     }
 }
